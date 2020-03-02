fa7635285ce1f1a0adb402b7ec5e87164aed8dd6
heyuantao <he_yuan_tao@163.com>
2020-03-02 16:13:55 +0800

    redis

--- database/redis.py
+++ database/redis.py
@@ -10,10 +10,7 @@ class RedisClient:
         connection_pool = redis.ConnectionPool(host='127.0.0.1',port=6370,db=0)
         redis_connection = redis.Redis(connection_pool=connection_pool)
         self.redis_connection = redis_connection
-        print("This is redis reference info {} at init".format(self))
 
-    def display(self):
-        print("This is redis reference info {} at display".format(self))
 
 
 
