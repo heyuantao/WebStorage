c09e01dfe745c1ec13c3239550ead57a39c66a48
heyuantao <he_yuan_tao@163.com>
2020-03-02 13:16:13 +0800

    change

--- database/redis.py
+++ database/redis.py
@@ -3,9 +3,12 @@ import redis
 from config import config
 
 class RedisClient:
+
     def init_app(self,app):
         connection_pool = redis.ConnectionPool(host='127.0.0.1',port=6370,db=0)
         redis_connection = redis.Redis(connection_pool=connection_pool)
         self.redis_connection = redis_connection
+
+
 
 
