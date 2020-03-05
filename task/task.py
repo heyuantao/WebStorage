dbb1b14cd7811daff4a1f7c783579464694193db
heyuantao <he_yuan_tao@163.com>
2020-03-05 09:30:54 +0800

    save

--- task/task.py
+++ task/task.py
@@ -1,21 +1,30 @@
 #-*- coding=utf-8 -*-
-from __future__ import absolute_import
-from utils import Singleton,MessageException
+#from __future__ import absolute_import
+
 from celery import Celery
+from celery.schedules import crontab
 from config import config
+from utils import Singleton,MessageException
 from db import Database
 from storage import Storage
 import logging
 import traceback
 
+logger = logging.getLogger(__name__)
 store = Storage()
 db = Database()
-
 celery = Celery("task", broker = config.App.CELERY_BACKEND)
 
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(1.0, do_something.s('hello'), name='add every 10')
+
 @celery.task
-def do_something():
-    print(db.get_file_list_cache())
+def do_something(msg):
+    print("schedule task:{}".format(msg))
+    logger.debug("schedule task:{}".format(msg))
+
 
 @celery.task
 def merge_file_by_key_in_celery(key):
