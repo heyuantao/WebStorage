04cbcb16e6685c25dbaf3cc6573f44a572802f7c
heyuantao <he_yuan_tao@163.com>
2020-03-08 17:50:32 +0800

    default token warning

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class AppConfig:
     SITE_URL = "http://127.0.0.1:5000"
     STATIC_FOLDER = "./templates/mystorageapp/build/static"  # not end with slash
     TEMPLATE_FOLDER = "./templates/mystorageapp/build"  # not end with slash
-    AUTH_TOKEN = ["UseMyWebStorageService","6ea4ef99ab4a56e322c2bea276d6337c"]  #可以设置多个token
+    AUTH_TOKEN = ["UseMyWebStorageService","6ea4ef99ab4a56e322c2bea276d6337c"]  #可以设置多个token,可以设置并修改
     CELERY_BACKEND = "redis://127.0.0.1:6379/1"
 
 @Singleton
@@ -23,6 +23,10 @@ class Config:
     App = AppConfig()
 
     def __init__(self):
-        logger.critical("The auth token is {}".format(self.App.AUTH_TOKEN))
+        default_token_list = ["UseMyWebStorageService","6ea4ef99ab4a56e322c2bea276d6337c"]
+        for token_item in default_token_list:
+            if token_item in self.App.AUTH_TOKEN:
+                logger.critical("The default auth token \"{}\" is using .This may cause secure problem !".format(token_item))
+
 
 config = Config()
