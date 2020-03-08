9cab4ae7853e1a44eb36c432763bbfcebb4c940a
heyuantao <he_yuan_tao@163.com>
2020-03-08 17:45:23 +0800

    change access token

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 #-*- coding=utf-8 -*-
 import os
 from utils import Singleton
+import logging
+
+logger = logging.getLogger(__name__)
 
 class FileStorageConfig:
     TMP_UPLOAD_PATH ="./data/tmp"                               #before merge
@@ -11,12 +14,15 @@ class AppConfig:
     SITE_URL = "http://127.0.0.1:5000"
     STATIC_FOLDER = "./templates/mystorageapp/build/static"  # not end with slash
     TEMPLATE_FOLDER = "./templates/mystorageapp/build"  # not end with slash
-    AUTH_TOKEN = ["1234567890","abcdefghi"]
+    AUTH_TOKEN = ["UseMyWebStorageService","6ea4ef99ab4a56e322c2bea276d6337c"]  #可以设置多个token
     CELERY_BACKEND = "redis://127.0.0.1:6379/1"
 
 @Singleton
 class Config:
     FileStorage = FileStorageConfig()
     App = AppConfig()
+
+    def __init__(self):
+        logger.critical("The auth token is {}".format(self.App.AUTH_TOKEN))
 
 config = Config()
